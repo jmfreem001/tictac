9f5300c84e8d64f4b701b79e877fd3a713d2631c
Josh Freeman <jmfreem001@gmail.com>
2019-07-31 22:06:19 -0500

    Add alternating turn and draw end condition.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,23 +3,29 @@ const board = ( () => {
     let array = [];
     let height = 3;
     let size = height * height;
-    for (let i=0; i< size; i++){
-        array.push('');
+
+    const fillBlanks = () => {
+        for (let i=0; i< size; i++){
+            array.push('');
+        }
     }
-    const show = () => console.log(array);
-    // Need to add a location
+
+    fillBlanks()
+
     const change = (id, move) => {
         //Add move board
         array.splice(id,1, move);
         console.log(`Board updated with ${move}`)
     }
     const clear = () => {
-        board = [];
-        console.log('Clearing board');
+        // Using the code breaks functionality of board. 
+        // array = [];
+        // fillBlanks()
+        // console.log('Clearing board');
+        console.log(array);
     }
     return { 
         array,
-        show,
         change,
         size,
         clear 
@@ -33,10 +39,17 @@ const controller = ( () => {
     let Xplayer = {};
     let Oplayer = {};
     let activePlayer = {};
+    let log = document.getElementById('log');
     // Update game
     const makeMark = (e) => {
         choice = e.target.dataset.id;
-        console.log(`Active player is ${activePlayer.name}`)
+        //Determine round number
+        markCount = board.array
+            .filter(item => item !== '').length;
+        // console.log(markCount);
+        activePlayer = setActivePlayer(markCount);
+        // console.log(`Active player is ${activePlayer.name}`)
+
         if (board.array[choice] !== ''){
             alert('Already taken! Please choose a different square.');
             return;
@@ -44,10 +57,31 @@ const controller = ( () => {
         if (activePlayer) {
             activePlayer.move(choice); 
         }
+        // Check if player hasWon()
+        if ((markCount + 1) === board.array.length) {
+            end(activePlayer, 'draw');
+            return;
+        }
+        else if (hasWon(activePlayer)){
+            end(activePlayer, 'win');
+            return
+        }
+        // Update diplay to prepare for next mark
+        activePlayer = setActivePlayer(markCount + 1);
+
+        displayNewPlayer(activePlayer)
+
         render()
-        changeActivePlayer(activePlayer)
+
+
+
         // TODO finish implementation of mark.
 
+    }
+
+    const setActivePlayer = (round) => {
+        // update active player based on round
+        return ((round) % 2 === 0)? Xplayer: Oplayer;
     }
 
     const render = () => {
@@ -74,48 +108,62 @@ const controller = ( () => {
 
     // render game board for each element in size array. 
     const start = () => {
+        // Reset board befor e starting
+        board.clear()
         render()
         // Get PLayer names or use a default value
         let xName = document.getElementById('xName').value || 'John Doe';
         Xplayer = createPlayer(xName, 'X');
         let oName = document.getElementById('oName').value || 'Jane Doe';        
         Oplayer = createPlayer(oName, 'O');
-        activePlayer = Xplayer;
-        mainLoop()
+        // activePlayer = Xplayer;
+        displayNewPlayer(Xplayer)
+        // mainLoop(activePlayer)
 
     }
-    const changeActivePlayer = (activePlayer) =>  {
-        // CUREENTLY A BUG IN THIS CODE WHERE active playe only changes within block andn then reverts back. 
-        activePlayer = (activePlayer === Xplayer)? Oplayer:Xplayer;
-        console.log('change active player called')
+    const displayNewPlayer = (activePlayer) =>  {
+
+        log.textContent = `It is your turn to move ${activePlayer.name}`
+  
+        return;
     }
     
-    const mainLoop = () => {
-        let activePlayer = Xplayer
-        let active = true;
-        let moved = false;
-        while (active) {
-            // Update status element stating that 
-            console.log(`It is your turn ${activePlayer.name}.`)
 
-            // TODO may need and event listener here to listent for when the board change is done
-            // Then check if player has won or if the board is full and no one has won. and announce win or draw
-            // Then render screen if not the end. 
-            // Other wise switch active player to other player
-            // activePlayer = changeActivePlayer(activePlayer)
-            console.log(activePlayer)
-            // Temporary while building the rest of the logic to make sure the loop ends
-            active = false;
+    const hasWon = (activePlayer) => {
+        // Check board to see if there are three in a row.
+        // .log(textContent)
+
+        // return true; 
+    };
+
+    const end = (activePlayer, winStatus) => {
+        console.log(`The game is over`);
+        // If win state player has won
+        if (winStatus === 'win'){
+            let log = document.getElementById('log');
+            log.textContent = `${activePlayer.name} has won!`;
+            console.log('Win!')
+
+            return;
+        }
+        else if (winStatus === 'draw') {
+            let log = document.getElementById('log');
+            log.textContent = 'Looks like a draw. CLick New game to start again.';
+            console.log('draw')
+            render()
+            return;
+        }
+        else {
+            alert('Oops! Something went wrong!');
+            return;
         }
     }
-
-    const end = () => console.log('The game is over');
-
     return {
         start,
         end,
         activePlayer
-    }
+        }
+    
 })();
 
 
